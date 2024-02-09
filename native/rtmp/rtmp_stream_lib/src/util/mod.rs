71249e5d82c32f301d3433a69d477aae5ecff1fa
Iaroslav Dochien <iaroslav@atomos.com>
2024-02-10 05:44:12 +1100

    Added ` src/util`

---
+++ native/rtmp/rtmp_stream_lib/src/util/mod.rs
@@ -0,0 +1,11 @@
+pub mod util;
+
+macro_rules! trace {
+    ($($args: expr),*) => {
+        print!("TRACE: file: {}, line: {}", file!(), line!());
+        $(
+            print!(", {}: {}", stringify!($args), $args);
+        )*
+        println!(""); // to get a new line at the end
+    }
+}
